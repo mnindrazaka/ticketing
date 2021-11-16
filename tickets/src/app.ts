260fb6fe36f813bd043d196370d2d90e8d11e910
M. Nindra Zaka <mnindrazaka@gmail.com>
2021-11-17 05:46:03 +0700

    chore: setup tickets service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import { currentUserRouter } from "./routes/currentUser";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
 import { errorHandler, NotFoundError } from "@mnindrazakatickets/common";
 import cookieSession from "cookie-session";
 
@@ -10,6 +14,11 @@ app.use(json());
 app.use(
   cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
 );
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
