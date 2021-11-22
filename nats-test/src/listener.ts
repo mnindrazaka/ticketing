3403fd63914b34b80e90a51dea1dd2c423f5e37d
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2021-11-23 06:17:02 +0700

    feat: properly shutdown nats client

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,13 +1,26 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing", "123", { url: "http://localhost:4222" });
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
+  url: "http://localhost:4222",
+});
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS connection closed!");
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
@@ -15,5 +28,10 @@ stan.on("connect", () => {
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()} with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
